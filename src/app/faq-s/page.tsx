3bf2cd310a462ce27fcf0984d38a50b06fe6f4ed
Alfredo Dumaine <adumaine@grupoautofin.com>
2025-03-18 17:09:22 -0600

    fix: image mobil faqs

--- src/app/faq-s/page.tsx
+++ src/app/faq-s/page.tsx
@@ -11,12 +11,12 @@ const bebasNeue = Bebas_Neue({
 export default function FAQs() {
   return (
     <main className="min-h-screen bg-white dark:bg-black">
-      <FaqsImage desktopSrc="/SERVICE/1.jpg" mobileSrc="/SERVICE/mobil1.jpeg" desktopHeight="h-[500px]" mobileHeight="h-[400px]" />
+      <FaqsImage desktopSrc="/SERVICE/1.jpg" mobileSrc="/SERVICE/mobil1.jpeg" desktopHeight="h-[500px]" mobileHeight="h-[300px]" />
       <div className="text-center pt-12">
         <h1 className={`text-7xl font-bold text-[#0239E2] ${bebasNeue.className}`}>FAQ&apos;S</h1>
       </div>
       <FaqAccordion />
-      <FaqsImage desktopSrc="/SERVICE/2.jpg" mobileSrc="/SERVICE/mobil2.jpeg" desktopHeight="h-[600px]" mobileHeight="h-[400px]" />
+      <FaqsImage desktopSrc="/SERVICE/2.jpg" mobileSrc="/SERVICE/mobil2.jpeg" desktopHeight="h-[600px]" mobileHeight="h-[300px]" />
       <QuestionSection />
     </main>
   )
